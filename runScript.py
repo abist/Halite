b102fcac55443a9897b43055c4c72be80ae34468
Aditya Bist <abist@andrew.cmu.edu>
2016-11-16 02:17:42 -0800

    able to play 10 games and log results using script

--- runScript.py
+++ runScript.py
@@ -1,5 +1,9 @@
 import subprocess
+import sys
 
+file_out = open("./game.log", "w")
 for i in xrange(10):
-    subprocess.call(['./runGame.sh'])
+    subprocess.call(['./runGame.sh'], stdout=file_out)
+    sys.stdout.write("Finished Game %d\n" % (i+1))
+
 
