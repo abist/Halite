c3112a6cb493f12c7e9b1308b0eeb0c6b469e020
Aditya Bist <abist@andrew.cmu.edu>
2016-11-16 17:20:03 -0800

    script now shows result on stdout

--- runScript.py
+++ runScript.py
@@ -1,9 +1,14 @@
 import subprocess
 import sys
 
+maxIndex = 0
 file_out = open("./game.log", "w")
-for i in xrange(10):
+for i in xrange(3):
     subprocess.call(['./runGame.sh'], stdout=file_out)
     sys.stdout.write("Finished Game %d\n" % (i+1))
+    game_file = open("./game.log", "r")
+    for line in game_file:
+        if ("Player" in line):
+            sys.stdout.write(line +"\n")
 
 
